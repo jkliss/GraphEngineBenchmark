1ccc28bc0fddce86a8805561e5d70a44e42ea5f7
julius <11009200+jkliss@users.noreply.github.com>
2019-08-05 19:49:26 +0200

    .

--- BenchmarkServer/SimpleBenchmarkServer.cs
+++ BenchmarkServer/SimpleBenchmarkServer.cs
@@ -316,7 +316,7 @@ namespace BenchmarkServer
                 if (cell.Depth > request.level + 1) {
                   cell.Depth = request.level + 1;
                   cell.parent = request.senderId;
-                  Console.WriteLine("CELL:" + cell.ID + " DEPTH:" + cell.Depth);
+                  Console.WriteLine("CELL:" + cellId + " DEPTH:" + cell.Depth);
                   List<long> aliveNeighbors = new List<long>();
                   for (int i = 0; i < cell.Outlinks.Count; i++) {
                     if (Global.CloudStorage.Contains(cell.Outlinks[i])) {
