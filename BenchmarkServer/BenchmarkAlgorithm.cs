d284a1cca620158cd4e7696b62e1575d92a8b4ab
julius <11009200+jkliss@users.noreply.github.com>
2019-08-06 19:30:35 +0200

    .

--- BenchmarkServer/BenchmarkAlgorithm.cs
+++ BenchmarkServer/BenchmarkAlgorithm.cs
@@ -222,7 +222,6 @@ namespace BenchmarkServer
                 long outlink = (long) response.Outlinks[i];
                 int output_int = (int) outlink;
                 Console.WriteLine("Cell " + outlink);
-                Console.WriteLine("Link " + response.link);
                 Console.WriteLine("CNODE " + current_node + " has depth " + depth[current_node]);
                 if (depth[output_int] > depth[current_node] + 1){
                   depth[output_int] = depth[current_node] + 1;
